93ce03b4c2702a6c691eb454f28ee2cf420d5757
Suzakri <193090173+Suzakri@users.noreply.github.com>
2025-01-05 17:28:45 -0500

    Added unit tests for Vector operations

--- vector/src/operations.rs
+++ vector/src/operations.rs
@@ -37,3 +37,66 @@ impl Vector {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+  use super::super::types::Vector;
+
+  #[test]
+  fn test_scale_tuple2d() {
+    let v = Vector::Tuple2D(1, 2);
+    let scaled = v.scale(2);
+    
+    match scaled {
+      Vector::Tuple2D(x, y) => {
+        assert_eq!(x, 2);
+        assert_eq!(y, 4);
+      }
+      _ => panic!("Error scaling vector type")
+    }
+  }
+
+  #[test]
+  fn test_scale_tuple3d() {
+    let v = Vector::Tuple3D(1, 2, 3);
+    let scaled = v.scale(2);
+    
+    match scaled {
+      Vector::Tuple3D(x, y, z) => {
+        assert_eq!(x, 2);
+        assert_eq!(y, 4);
+        assert_eq!(z, 6);
+      }
+      _ => panic!("Error scaling vector type")
+    }
+  }
+
+  #[test]
+  fn test_scale_struct2d() {
+    let v = Vector::Struct2D {x: 1, y: 2};
+    let scaled = v.scale(2);
+    
+    match scaled {
+      Vector::Struct2D {x, y} => {
+        assert_eq!(x, 2);
+        assert_eq!(y, 4);
+      }
+      _ => panic!("Error scaling vector type")
+    }
+  }
+
+  #[test]
+  fn test_scale_struct3d() {
+    let v = Vector::Struct3D {x: 1, y: 2, z: 3};
+    let scaled = v.scale(2);
+    
+    match scaled {
+      Vector::Struct3D {x, y, z} => {
+        assert_eq!(x, 2);
+        assert_eq!(y, 4);
+        assert_eq!(z, 6);
+      }
+      _ => panic!("Error scaling vector type")
+    }
+  }
+}
